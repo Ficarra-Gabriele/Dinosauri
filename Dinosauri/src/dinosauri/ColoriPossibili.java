4ba25e95ee08fbb7177331da9f7d45564bcddc95
Ficarra-Gabriele <ficarra.gabriele@franchettisalviani.net>
2026-02-17 13:33:09 +0100

    Versione 1.1
    Ho creato le classi

---
+++ Dinosauri/src/dinosauri/ColoriPossibili.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dinosauri;
+
+/**
+ *
+ * @author ficarra.gabriele
+ */
+public enum ColoriPossibili {
+    ROSSO,
+    GIALLO,
+    VERDE
+}
